8f5188f95e517dfd842f1bae6b8ae7fcda586a99
AminHossain <aminhossain283@gmail.com>
2025-01-03 23:52:04 +0600

    feat: rendaring, conditional rendaring

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,22 @@
 function ListGroup() {
-    return <div>List Group</div>
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London'
+    ]
+    
+    return (
+        <>
+            <h1>List Group</h1>
+            {items.length === 0 && <h3>No items found!</h3>}
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li className="list-group-item" key={item}>{item}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default ListGroup
